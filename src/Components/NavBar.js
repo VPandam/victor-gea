26ab2d8722fe050912272d88a92ea91cadf2e017
Pandam <victorvgv96@hotmail.es>
2021-06-03 18:33:23 +0200

    NavBar responsive and pictures portfolio

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -4,17 +4,40 @@ import Panda from '../Images/panda.png'
 
 import {Link} from 'react-scroll'
 class NavBar extends React.Component{
-   render() {
+    state = {clicked: false}
+
+    handleClick = () => {
+        this.setState({clicked:!this.state.clicked})
+    }
+    
+    render() {
     return (
         <React.Fragment>
 
-            <div className='NavBar'>
-                    <ul className='NavBar__list'>
-                    <li className='NavBar__list-li'><Link  to="home" spy={true} smooth={true}>Home</Link></li>
-                    <li className='NavBar__list-li'><Link  to="about" spy={true} smooth={true} >About me</Link></li>
-                    <li className='NavBar__list-li'><Link  to="skills" spy={true} smooth={true} >Skills</Link></li>
-                    <li className='NavBar__list-li'><Link  to="portfolio" spy={true} smooth={true} >Portfolio</Link></li>
-                    <li className='NavBar__list-li'><Link  to="contact" spy={true} smooth={true} >Contact</Link></li>
+            <div className='navBar'>
+                
+                    <button className={this.state.clicked ?'button-menu-clicked' : 'button-menu' } 
+                    onClick={this.handleClick} ></button>
+                
+                <ul className={this.state.clicked ?'navBar__list-clicked' : 'navBar__list' }> 
+                    <li className='navBar__list-li'>
+                        <Link to="home" spy={true} smooth={true}>
+                            Home</Link></li>
+
+                    <li className='navBar__list-li'>
+                        <Link to="about" spy={true} smooth={true} >
+                            About me</Link></li>
+
+                    <li className='navBar__list-li'>
+                        <Link to="skills" spy={true} smooth={true}  offset={-100} >
+                            Skills</Link></li>
+
+                    <li className='navBar__list-li'>
+                        <Link to="portfolio" spy={true} smooth={true} >
+                            Portfolio</Link></li>
+                    <li className='navBar__list-li'>
+                        <Link to="contact" spy={true} smooth={true} >
+                            Contact</Link></li>
                 </ul>          
             </div>
         </React.Fragment>
