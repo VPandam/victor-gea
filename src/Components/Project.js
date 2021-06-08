d52edff4de7579777b7826591e1f5a60454a4a04
Pandam <victorvgv96@hotmail.es>
2021-06-08 18:33:31 +0200

    Improvement on closing modals function

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -11,58 +11,38 @@ class Portfolio extends React.Component{
 
         
         modal.style.display='block';
+        this.props.onModalClick(this.props.id)
         
 
     }
 
     // When the user clicks on <span> (x), close the modal
-    closeModal() {
-        let modal = document.getElementById('myModal' + this.props.id); 
+    // closeModal() {
+    //     let modal = document.getElementById('myModal' + this.props.id); 
 
-        modal.style.display = "none";
+    //     modal.style.display = "none";
     
-    }   
+    // }   
 
+  componentDidMount(){
+    
+
+  }
   
-  // When the user clicks anywhere outside of the modal, close it
-  // Id of the project needs to be declared for this code to work
-    componentDidMount(){
-        let modal1 = document.getElementById('myModal1')
-        let modal2 = document.getElementById('myModal2')
-        let modal3 = document.getElementById('myModal3')
-        let modal4 = document.getElementById('myModal4')
-
-        
-        
-        window.onclick = function(event) {
-
-            if(event.target == modal1 || event.target == modal2
-                || event.target == modal3 || event.target == modal4){
-                modal1.style.display = "none";
-                modal2.style.display = "none";
-                modal3.style.display = "none";
-                modal4.style.display = "none";
-
-            }
-
-            
-        }
-    }
 
 
     render(){
 
-
+        
 
         return(
 
             <div className={'project ' + this.props.projectClass}>
-                <a onClick={this.openModal.bind(this)} ></a>
-
+                <a id='etiqueta' onClick={this.openModal.bind(this)} ></a>
                 <div id={'myModal' + this.props.id} className="modal">
 
                 <div className="modal-content">
-                    <span onClick={this.closeModal.bind(this)} className="close">&times;</span>
+                    <span id={'close-modal-span' + this.props.id} className="close">&times;</span>
                     
                     <div className='modal-content__flex'>
                         
--- src/Pages/Portfolio.js
+++ src/Pages/Portfolio.js
@@ -4,13 +4,39 @@ import Project from '../Components/Project'
 
 class Portfolio extends React.Component{
 
-    state = {modalId: 0}
 
-    handleClick(e){
-        this.setState({modalId: e.target.nextElementSibling})
+    componentDidMount(){
+        let modals = document.getElementsByClassName("modal");
+        console.log(modals);
+
+         // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+                
+            for (let index = 0; index < modals.length; index++) {
+                if(event.target == modals[index]){
+                    event.target.style.display = 'none';
+                }
+                
+            } 
+           
+        }
+        
+ 
     }
 
+    onModalClick(id){
+        let modal = document.getElementById("myModal" + id );
+        let closeSpan = document.getElementById('close-modal-span' + id)
 
+         // When the user clicks anywhere outside of the modal or in the close labgel, close the modal.
+        window.onclick = function(event) {
+            
+            if(event.target == modal || event.target == closeSpan){
+                
+                modal.style.display = 'none';
+            }
+        }
+    }
 
     render(){
         return(
@@ -22,27 +48,33 @@ class Portfolio extends React.Component{
                 </div>
 
                 <div className='project__container'>
-                    <div onClick={this.handleClick.bind(this)}>
-                        <Project 
-                            projectClass='mobile-first'
-                            projectDescription='Its the website of Batatabit, an app that let you buy and compare crypto. 
-                            In the website you can consult the different plans, whatch graphics and learn about
-                            what the app offers you.
-                            This is a project i made for learning html and css.
-                            Its part of the frontend developer school in platzi, an online academy.'
-                            id = '1'
-                            
-                        />
-                    </div>
                     
+                    <Project 
+                        projectClass='mobile-first'
+                        projectDescription='Its the website of Batatabit, an app that let you buy and compare crypto. 
+                        In the website you can consult the different plans, whatch graphics and learn about
+                        what the app offers you.
+                        This is a project i made for learning html and css.
+                        Its part of the frontend developer school in platzi, an online academy.'
+                        id = '1'
+                        onModalClick = {this.onModalClick}
+                        
+                    />                  
                     <Project
                         projectClass='space-man'
                         projectDescription='Its a very simple game made for learning Unity with C#. I learnt
                         about object oriented programming and rendering in real time.'
                         id = '2'
+                        onModalClick = {this.onModalClick}
                     />
-                    <Project id = '3'/>              
-                    <Project id = '4' />              
+                    <Project 
+                        id = '3'
+                        onModalClick = {this.onModalClick}
+                    />              
+                    <Project 
+                        id = '4' 
+                        onModalClick = {this.onModalClick}
+                    />              
                 </div>
                 
             </div>
